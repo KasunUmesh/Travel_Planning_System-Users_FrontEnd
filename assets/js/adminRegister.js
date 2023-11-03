4c3a5416c9fe7f608f1bb0f67005d56c97437c4d
KasunUmesh <kasunumesh38@gmail.com>
2023-11-03 08:53:15 +0530

    register form created

--- assets/js/adminRegister.js
+++ assets/js/adminRegister.js
@@ -1,20 +1,33 @@
 // Image Upload and view
 
-const imgDiv = document.querySelector(".profileIMG-wrapper");
-const img = document.querySelector("#photo");
-const file = document.querySelector("#adminIMG");
-const uploadbtn = document.querySelector("#uploadbtn");
+const inputFile1 = document.querySelector("#input-file1");
+const imageView1 = document.querySelector("#img-view1");
 
-file.addEventListener("change", function () {
-  const chosedfile = this.files[0];
-  if (chosedfile) {
-    const reader = new FileReader();
+const inputFile2 = document.querySelector("#input-file2");
+const imageView2 = document.querySelector("#img-view2");
 
-    reader.addEventListener("load", function () {
-      img.setAttribute("src", reader.result);
-    });
-    reader.readAsDataURL(chosedfile);
-  }
+const inputFile3 = document.querySelector("#input-file3");
+const imageView3 = document.querySelector("#img-view3");
+
+inputFile1.addEventListener("change", function () {
+  let imgLink = URL.createObjectURL(inputFile1.files[0]);
+  imageView1.style.backgroundImage = `url(${imgLink})`;
+  imageView1.textContent = "";
+  imageView1.style.border = 0;
+});
+
+inputFile2.addEventListener("change", function () {
+  let imgLink = URL.createObjectURL(inputFile2.files[0]);
+  imageView2.style.backgroundImage = `url(${imgLink})`;
+  imageView2.textContent = "";
+  imageView2.style.border = 0;
+});
+
+inputFile3.addEventListener("change", function () {
+  let imgLink = URL.createObjectURL(inputFile3.files[0]);
+  imageView3.style.backgroundImage = `url(${imgLink})`;
+  imageView3.textContent = "";
+  imageView3.style.border = 0;
 });
 
 // Frontend and backend functions

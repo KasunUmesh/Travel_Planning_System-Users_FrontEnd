5ee1a9b0e10c055a63c2a42ad5046443733198c4
KasunUmesh <kasunumesh38@gmail.com>
2024-01-20 13:52:58 +0530

    user login backend connect

--- assets/js/userLogin.js
+++ assets/js/userLogin.js
@@ -1,62 +1,62 @@
 // Login Text Field Validation
 
 function loginFieldValidation() {
-  const email = $("#adminEmail").val();
-  const password = $("#adminPassword").val();
+  const email = $("#userEmail").val();
+  const password = $("#userPassword").val();
 
   if (email == "") {
     $("#emailError").css("display", "block");
-    $("#adminEmail").css("border-color", "#e72438");
+    $("#userEmail").css("border-color", "#e72438");
   }
 
   if (password == "") {
     $("#passwordError").css("display", "block");
-    $("#adminPassword").css("border-color", "#e72438");
+    $("#userPassword").css("border-color", "#e72438");
   }
 }
 
 $(document).ready(function () {
-  $("#adminEmail").on("input", function () {
-    var email = $("#adminEmail").val();
+  $("#userEmail").on("input", function () {
+    var email = $("#userEmail").val();
     let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
     var matchResult = email.match(pattern);
 
     if (matchResult) {
       $("#emailError").css("display", "none");
-      $("#adminEmail").css("border", "2px solid rgba(255, 255, 255, .2)");
+      $("#userEmail").css("border", "2px solid rgba(255, 255, 255, .2)");
     } else {
       $("#emailError").css("display", "block");
-      $("#adminEmail").css("border-color", "#e72438");
+      $("#userEmail").css("border-color", "#e72438");
     }
   });
 
-  $("#adminPassword").on("input", function () {
-    var email = $("#adminPassword").val();
+  $("#userPassword").on("input", function () {
+    var email = $("#userPassword").val();
 
     if (email == "") {
       $("#passwordError").css("display", "block");
-      $("#adminPassword").css("border-color", "#e72438");
+      $("#userPassword").css("border-color", "#e72438");
     } else {
       $("#passwordError").css("display", "none");
-      $("#adminPassword").css("border", "2px solid rgba(255, 255, 255, .2)");
+      $("#userPassword").css("border", "2px solid rgba(255, 255, 255, .2)");
     }
   });
 });
 
-// Admin Login Function
+// User Login Function
 
-var loginBaseUrl = "";
+var loginBaseUrl = "http://localhost:8085/travelPlanning/user/userlogin";
 
-$("#adminLoginBtn").click(function () {
+$("#userLoginBtn").click(function () {
   loginFieldValidation();
-  adminLogin();
+  userLogin();
 });
 
-function adminLogin() {
+function userLogin() {
   var data = {
-    adminEmail: $("#adminEmail").val(),
-    adminPassword: $("#adminPassword").val(),
+    email: $("#userEmail").val(),
+    password: $("#userPassword").val(),
   };
 
   $.ajax({
@@ -67,7 +67,7 @@ function adminLogin() {
     contentType: "application/json",
     success: function (res) {
       if (res.status == true) {
-        window.location.href = "";
+        window.location.href = "../pages/package.html";
       } else {
         if (res.message != "Password Not Match") {
           $("#emailError").text(res.message);
